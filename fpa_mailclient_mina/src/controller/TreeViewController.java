29e99c277f31c817755723836ab0200990b45c03
animina <animina@riseup.net>
2015-05-05 17:10:28 +0200

    updates

---
+++ fpa_mailclient_mina/src/controller/TreeViewController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TreeView;
+
+import javax.swing.tree.TreeNode;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by mina on 05.05.15.
+ */
+public class TreeViewController implements Initializable {
+
+    @FXML
+    private TreeView fileTree = new TreeView();
+
+    @Override
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
--- fpa_mailclient_mina/src/view/Main.java
+++ fpa_mailclient_mina/src/view/Main.java
@@ -4,14 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("window.fxml"));
+        BorderPane root = FXMLLoader.load(getClass().getResource("window.fxml"));
+        primaryStage.getIcons().add(new Image("Mail-icon.png"));
         primaryStage.setTitle("Endlich Mails");
+        TreeView treeViewWindow = FXMLLoader.load(getClass().getResource("../view/treeView.fxml"));
+        root.setLeft(treeViewWindow);
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
